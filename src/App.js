2f69308284f34cde0cd217a493a6c3b30ae024e2
HamdiRabeh <46111136+Rabehhamdi@users.noreply.github.com>
2019-07-05 14:32:25 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Time from './Component/time.js'
+import Timer from './Component/timer'
 
 function App() {
-  return (
-    <div className="App"> 
-        <Time /> 
-    </div>
+  return ( 
+   <div>
+      <Timer /> 
+  </div>
   );
 }
 
--- src/Component/time.js
+++ src/Component/time.js
@@ -1,54 +1,31 @@
-import React from 'react';
+import React from 'react'
 
+const msToTime = ms =>{
+    const msPerSecond = 1000
+    const msPerMinute = msPerSecond * 60
+    const msPerHeur = msPerMinute*60 
+    const Heur = Math.floor(ms / msPerHeur)
+    const HeurReste = ms % msPerHeur 
+    const Minute = Math.floor(HeurReste / msPerMinute)
+    const MinuteReste = HeurReste % msPerMinute
+    const Second = Math.floor(MinuteReste / msPerSecond)
+    return (String(Heur).padStart(2, '0') + ":" + String(Minute).padStart(2, '0') + ":" + String(Second).padStart(2, '0'))
 
-
-function Second(props) {  
-    var reste = 76400;  
-    if (reste <= 86400 )  
-    {
-        var result = '';
-
-         
-
-        var nbHours = Math.floor(reste / 3600);
-        reste -= nbHours * 3600;
-
-        var nbMinutes = Math.floor(reste / 60);
-        reste -= nbMinutes * 60;
-
-        var nbSeconds = reste;
-
-         
-
-        if (nbHours > 0)
-            result = result + nbHours + 'h ';
-
-        if (nbMinutes > 0)
-            result = result + nbMinutes + 'min ';
-
-        if (nbSeconds > 0)
-            result = result + nbSeconds + 's ';
-        }
-    else 
-        result ="reset <= 86400 "
-    return(
-        <div>
-            {result}
-        </div>
-    )
-}
- 
-
-function Time() {
-
-    return (
-        <div>
-            
-            <Second  />
-             
-             
-        </div>
-    );
 }
 
-export default Time;
+const Time = ({ ms }) => {
+    return <div className="time-container">
+        <div className="time-inner-container">
+            <div className="time-digits">
+                {msToTime(ms)}
+            </div>
+            <div className="time-text">
+                <div className="time-text-item">Hour</div>
+                <div className="time-text-item">Minute</div>
+                <div className="time-text-item">Second</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Time
